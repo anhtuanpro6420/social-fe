21151ba7763ee5d75b1251d4635f0886f75882bb
tuanhoang <tuan.hoang@xenia.tech>
2019-09-28 11:42:13 +0700

    add token headers

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,11 @@
 import axios from 'axios';
+import storageService from './core/services/storageService';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:4000/api'
+	baseURL: 'http://localhost:4000/api'
 });
+instance.defaults.headers.common[
+	'Authorization'
+] = storageService.getAuthToken();
 
 export default instance;
