ac38598a7ba41c3b7bb84a3101685f1be8177a4e
tuanhoang <tuan.hoang@xenia.tech>
2019-09-28 11:47:01 +0700

    detect header login logout

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -17,9 +17,17 @@ const header = () => (
 			<Menu.Item key="1">
 				<Link to="/products">Home</Link>
 			</Menu.Item>
-			<Menu.Item key="2">
-				<Link to="/auth/login">Login</Link>
-			</Menu.Item>
+			{storageService.getCurrentUser() ? (
+				<Menu.Item key="2">
+					<Link to="/auth/login" onClick={storageService.removeAuth}>
+						Logout
+					</Link>
+				</Menu.Item>
+			) : (
+				<Menu.Item key="2">
+					<Link to="/auth/login">Login</Link>
+				</Menu.Item>
+			)}
 			<Menu.Item key="3">
 				<Link to="/sign-up">Sign up</Link>
 			</Menu.Item>
