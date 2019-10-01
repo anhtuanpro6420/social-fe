b5821b52693074b1422eba30201db1ab29dda029
tuanhoang <tuan.hoang@xenia.tech>
2019-10-01 23:57:37 +0700

    fix an error

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-	baseURL: process.env.REACT_APP_BASE_URI || 'http://localhost:4000/api';
+	baseURL: process.env.REACT_APP_BASE_URI || 'http://localhost:4000/api'
 });
 
 export default instance;
