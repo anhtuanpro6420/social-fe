30e2e6939959a7bb4c5ca271c7c5995f280fd69a
tuanhoang <tuan.hoang@xenia.tech>
2019-10-01 23:45:24 +0700

    edit environment variables

--- src/axios.js
+++ src/axios.js
@@ -1,10 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-	baseURL:
-		process.env.NODE_ENV !== 'production'
-			? 'http://localhost:4000/api'
-			: process.env.REACT_APP_BASE_URI
+	baseURL: process.env.REACT_APP_BASE_URI || 'http://localhost:4000/api';
 });
 
 export default instance;
