3480304bb322aa5f3546f0ce93600790b08b39b2
tuanhoang <tuan.hoang@xenia.tech>
2019-10-01 22:41:58 +0700

    update token config

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Switch, Redirect } from 'react-router-dom';
 import './App.css';
 import 'antd/dist/antd.css';
 import Newsfeed from './containers/Newsfeed/Newsfeed';
+import storageService from './core/services/storageService';
 
 const Signup = React.lazy(() => import('./containers/Signup/Signup'));
 const Login = React.lazy(() => import('./containers/Login/Login'));
@@ -10,39 +11,47 @@ const ShareVideo = React.lazy(() =>
 	import('./containers/ShareVideo/ShareVideo')
 );
 
-function App() {
-	let routes = (
-		<Switch>
-			<Route
-				path="/auth/login"
-				render={() => (
-					<Suspense fallback={<div>Loading...</div>}>
-						<Login />
-					</Suspense>
-				)}
-			/>
-			<Route
-				path="/sign-up"
-				render={() => (
-					<Suspense fallback={<div>Loading...</div>}>
-						<Signup />
-					</Suspense>
-				)}
-			/>
-			<Route
-				path="/share"
-				render={() => (
-					<Suspense fallback={<div>Loading...</div>}>
-						<ShareVideo />
-					</Suspense>
-				)}
-			/>
-			<Route path="/" exact component={Newsfeed} />
-			<Redirect to="/" />
-		</Switch>
-	);
+class App extends React.Component {
+	componentDidMount() {
+		const token = storageService.getAuthToken();
+		const email = storageService.getCurrentUser();
+		storageService.setAuth(token, email);
+		storageService.setAuthToken();
+	}
+	render() {
+		let routes = (
+			<Switch>
+				<Route
+					path="/auth/login"
+					render={() => (
+						<Suspense fallback={<div>Loading...</div>}>
+							<Login />
+						</Suspense>
+					)}
+				/>
+				<Route
+					path="/sign-up"
+					render={() => (
+						<Suspense fallback={<div>Loading...</div>}>
+							<Signup />
+						</Suspense>
+					)}
+				/>
+				<Route
+					path="/share"
+					render={() => (
+						<Suspense fallback={<div>Loading...</div>}>
+							<ShareVideo />
+						</Suspense>
+					)}
+				/>
+				<Route path="/" exact component={Newsfeed} />
+				<Redirect to="/" />
+			</Switch>
+		);
 
-	return <>{routes}</>;
+		return <>{routes}</>;
+	}
 }
 
 export default App;
